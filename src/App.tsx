61ab857841ece55e999f6ead60a6bded09fd95f7
eunseok-Hwang <chwolog1115@gmail.com>
2025-09-01 09:52:49 +0900

    .

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { AppBar, Container, CssBaseline, Toolbar, Typography } from "@mui/material"
+
+
+
+function App() {
+  return(
+  <>
+    <Container maxWidth='xl'>
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">
+            자동차 쇼핑몰
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Container>
+  </>
+  )
+}
+
+export default App
---
+++ src/api/carApi.tsx
@@ -0,0 +1,8 @@
+//자동차 목록 조회
+export const getCars = (): Car[] => {
+    //서버로 데이터 조회 ==> 응답 데이터 리턴
+    const getCargDummy = {
+
+    }
+
+}
